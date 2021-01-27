8120262998b850a0e8a93f0cb6c70f9403b19da7
Ruslana Logosha <ruslanalogosha@gmail.com>
2021-01-27 19:27:49 +0200

    adds views

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { Switch, Route } from 'react-router-dom';
-import ContactsView from './views/ContactsView';
-import AppBar from './components/AppBar';
 import Container from './components/Container';
+import AppBar from './components/AppBar';
+import HomeView from './views/HomeView';
+import RegisterView from './views/RegisterView';
+import LoginView from './views/LoginView';
+import ContactsView from './views/ContactsView';
 
 export default function App() {
   return (
     <Container>
       <AppBar />
       <Switch>
-        {/* <Route exact path="/" component={HomeView} />
+        <Route exact path="/" component={HomeView} />
         <Route path="/register" component={RegisterView} />
-        <Route path="/login" component={LoginView} /> */}
+        <Route path="/login" component={LoginView} />
         <Route path="/" component={ContactsView} />
       </Switch>
     </Container>
